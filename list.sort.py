78c811fa0d5c8db0380a4e28d68b02f4c01f7954
caramelthing <richard.palmer811@gmail.com>
2020-12-18 23:32:02 +0000

    updates to the file include x.sort(key.lower) like a methos in a method

--- list.sort.py
+++ list.sort.py
@@ -1,5 +1,25 @@
-#List type method, sort
+'''List type method, sort
 
 spam = [-2, 3.14, -7, 2, 9, 7]
 spam.sort()
 print(spam)
+
+zoo = ["zebra", "white lion","ant", "policeman"]
+zoo.sort()
+print(zoo)
+
+zoo.sort(reverse=True)
+print(zoo)
+
+#sorts the list in place, no good spam - spam.sort()
+#with a number and string value
+
+bag = ["books", "shiney new laptop", "the internet", 5, 3.69, "dam she's fine"]
+bag.sort()
+print(bag)
+
+#3.ASCIIbetical, capitaised Zoo, comes before lower case ant
+'''
+zoo = ["zebra", "white lion","ant", "policeman", "Zoo", "Xylophone playing Trump"]
+zoo.sort(key=str.lower)
+print(zoo)
